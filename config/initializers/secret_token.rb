fde8278e158f93facdc9cd86ae3b41479e6fc700
Your Name <jstevekaranja@gmail.com>
2012-07-30 19:33:47 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodaysApp::Application.config.secret_token = '783de27987becad8bdb8d0e93d6d34de0c70cb5aff8d65dd433ab410116fa6a2deb2ae9f8ec5a9e12c9db32ca23122e543af6ea29ba113c042599b710d322335'
